0b9b2714fc6188a2551f14981d53ba273d2632c3
Prasanthi <prasanti.bangi@gmail.com>
2024-08-19 16:10:07 -0500

    adding changes

--- Doc Reader/reader/reader.py
+++ Doc Reader/reader/reader.py
@@ -3,7 +3,6 @@ https://textract.readthedocs.io/en/stable/#
 Motivation : I have dome some research on the multiple libraries.
 https://pypdf.readthedocs.io/en/stable/index.html#, https://github.com/deanmalmgren/textract
 
-
 I found the above more useful as it supports multiple range of docs like word, pdf.
 
 Requirement :
@@ -11,12 +10,15 @@ Requirement :
    - check file extension
 
 Issues: textract does not work well with pdf so using PdfReader to read pdf
+TODO: test with more pages
+TODO: test with multiple file types
+TODO: Add exception handling
 """
 import magic
 import textract
 from dataclasses import dataclass
 from pypdf import PdfReader
-from enum import Enum
+
 
 def get_file_ext(file_path=str):
     """
@@ -52,15 +54,16 @@ class Reader:
             print("Processing Doc")
             if self.get_file_format(self.file).startswith(self.doc_word):
                 text = textract.process(self.file)
-                print(text)
+                # print(text)
                 return text
             elif self.get_file_format(self.file).startswith(self.doc_pdf):
                 reader = PdfReader(self.file)
                 number_of_pages = len(reader.pages)
-                print(number_of_pages)
-                page = reader.pages[0]
-                print(page, "-----")
+                # print(number_of_pages)
+                page = reader.pages[0] # defaulting to page 1, should handle more cases
+                # print(page, "-----")
                 text = page.extract_text()
+                return text
 
         else:
             print("Error - Cannot Process Document of Type : {}".format(self.get_file_format(self.file)))
